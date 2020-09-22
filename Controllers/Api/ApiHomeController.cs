68148b95b745602c808ac220f9851603911b43da
Harliin <56713048+Harliin@users.noreply.github.com>
2020-09-22 10:55:58 +0200

    Changes to home frontend and backend

--- Controllers/Api/ApiHomeController.cs
+++ Controllers/Api/ApiHomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Sinder.Helpers;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -30,14 +33,41 @@ namespace Sinder.Controllers.Api
 
         // POST api/<ApiHomeController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody] UserModel user)
         {
+            var ID = user.ID;
         }
 
         // PUT api/<ApiHomeController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> Put(int id)
         {
+            var cookies = Request.Cookies["token"];
+            string email = SecurityHelper.GetLoggedInUser(cookies);
+            UserModel loggedinUser = await Dataprovider.Instance.ReadUserByEmail(email);
+            if (loggedinUser.ID == id)
+            {
+                return new JsonResult(new ResponseModel { StatusCode = (int)HttpStatusCode.Unauthorized, Status = "Fail", Message = "Du kan inte skicka en vänförfrågan till dig själv!" }, new JsonSerializerOptions
+                {
+                    WriteIndented = true,
+                });
+            }
+
+            bool hasRelationship = await Dataprovider.Instance.CheckIfRelationshipExists(loggedinUser.ID, id);
+            if (hasRelationship == true)
+            {
+                return new JsonResult(new ResponseModel { StatusCode = (int)HttpStatusCode.Unauthorized, Status = "Fail", Message = "En vänförfrågan har redan skickats. Kolla dina vänförfrågningar!" }, new JsonSerializerOptions
+                {
+                    WriteIndented = true,
+                });
+            }
+
+            await Dataprovider.Instance.AddUserRelationship(loggedinUser.ID, id);
+
+            return new JsonResult(new ResponseModel { StatusCode = (int)HttpStatusCode.Unauthorized, Status = "Success", Message = "Vänförfrågan skickad!" }, new JsonSerializerOptions
+            {
+                WriteIndented = true,
+            });
         }
 
         // DELETE api/<ApiHomeController>/5
